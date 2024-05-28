8d8215aad994fabc7e23a15b9490996f2d4110b7
gudnuf <gudnuf21@proton.me>
2024-05-28 13:45:56 -0700

    finish

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,237 @@
+import useProofStorage from "@/hooks/useProofStorage";
+import { CashuMint, CashuWallet, getEncodedToken } from "@cashu/cashu-ts";
+import React, { useState, useEffect } from "react";
+
 const Wallet = () => {
+  const [formData, setFormData] = useState({
+    mintUrl: "",
+    mintAmount: "",
+    meltInvoice: "",
+    swapAmount: "",
+    swapToken: "",
+  });
+  const [dataOutput, setDataOutput] = useState(null);
+  /**
+   * @type {[CashuWallet|null, React.Dispatch<React.SetStateAction<CashuWallet|null>>]}
+   */
+  const [wallet, setWallet] = useState(null);
+
+  const { addProofs, balance, removeProofs, getProofsByAmount } =
+    useProofStorage();
+
+  useEffect(() => {
+    const storedMintData = JSON.parse(localStorage.getItem("mint"));
+    if (storedMintData) {
+      const { url, keyset } = storedMintData;
+      const mint = new CashuMint(url);
+
+      // initialize wallet with store keyset so we don't have to fetch them again
+      const wallet = new CashuWallet(mint, { keys: keyset, unit: "sat" });
+      setWallet(wallet);
+
+      setFormData((prevData) => ({ ...prevData, mintUrl: url }));
+    }
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSetMint = async () => {
+    const mint = new CashuMint(formData.mintUrl);
+
+    try {
+      const info = await mint.getInfo();
+      setDataOutput(info);
+      setWallet(new CashuWallet(mint));
+
+      const { keysets } = await mint.getKeys();
+
+      const satKeyset = keysets.find((k) => k.unit === "sat");
+
+      localStorage.setItem(
+        "mint",
+        JSON.stringify({ url: formData.mintUrl, keyset: satKeyset })
+      );
+    } catch (error) {
+      console.error(error);
+      setDataOutput({ error: "Failed to connect to mint", details: error });
+    }
+  };
+
+  const handleMint = async () => {
+    const amount = parseInt(formData.mintAmount);
+    const quote = await wallet.getMintQuote(amount);
+    setDataOutput(quote);
+
+    const intervalId = setInterval(async () => {
+      try {
+        const { proofs } = await wallet.mintTokens(amount, quote.quote, {
+          keysetId: wallet.keys.id,
+        });
+        setDataOutput({ "minted proofs": proofs });
+        setFormData((prevData) => ({ ...prevData, mintAmount: "" }));
+        addProofs(proofs);
+        clearInterval(intervalId);
+      } catch (error) {
+        console.error("Quote probably not paid: ", quote.request, error);
+        setDataOutput({ error: "Failed to mint", details: error });
+      }
+    }, 5000);
+  };
+
+  const handleMelt = async () => {
+    try {
+      const quote = await wallet.getMeltQuote(formData.meltInvoice);
+
+      setDataOutput([{ "got melt quote": quote }]);
+
+      const amount = quote.amount + quote.fee_reserve;
+
+      const proofs = getProofsByAmount(amount, wallet.keys.id);
+
+      if (proofs.length === 0) {
+        alert("Insufficient balance");
+        return;
+      }
+
+      const { isPaid, change } = await wallet.meltTokens(quote, proofs, {
+        keysetId: wallet.keys.id,
+      });
+
+      if (isPaid) {
+        removeProofs(proofs);
+        addProofs(change);
+      }
+    } catch (error) {
+      console.error(error);
+      setDataOutput({ error: "Failed to melt tokens", details: error });
+    }
+  };
+
+  const handleSwapSend = async () => {
+    const swapAmount = parseInt(formData.swapAmount);
+    const proofs = getProofsByAmount(swapAmount);
+
+    if (proofs.length === 0) {
+      alert("Insufficient balance");
+      return;
+    }
+
+    try {
+      const { send, returnChange } = await wallet.send(swapAmount, proofs);
+
+      const encodedToken = getEncodedToken({
+        token: [{ proofs: send, mint: wallet.mint.mintUrl }],
+      });
+
+      removeProofs(proofs);
+      addProofs(returnChange);
+      setDataOutput(encodedToken);
+    } catch (error) {
+      console.error(error);
+      setDataOutput({ error: "Failed to swap tokens", details: error });
+    }
+  };
+
+  const handleSwapClaim = async () => {
+    const token = formData.swapToken;
+
+    try {
+      const { token: newToken, tokensWithErrors } = await wallet.receive(token);
+
+      const { proofs } = newToken.token[0];
+
+      addProofs(proofs);
+      setDataOutput(proofs);
+    } catch (error) {
+      console.error(error);
+      setDataOutput({ error: "Failed to claim swap tokens", details: error });
+    }
+  };
+
   return (
     <main>
-      <div class="cashu-operations-container">
-        <div class="section">
-          <label for="mintu-url">Mintu URL:</label>
-          <input type="text" className="mintu-url" />
-          <button className="mint-connect-button">Set Mint</button>
+      <div className="cashu-operations-container">
+        <div className="section">
+          <label htmlFor="mint-url">Mint URL:</label>
+          <input
+            type="text"
+            name="mintUrl"
+            className="mint-url"
+            value={formData.mintUrl}
+            onChange={handleChange}
+          />
+          <button className="mint-connect-button" onClick={handleSetMint}>
+            Set Mint
+          </button>
         </div>
 
-        <div class="section">
+        <div className="section">
           <h2>Minting Tokens</h2>
-          <label for="mint-amount">Amount:</label>
-          <input type="number" id="mint-amount" className="mint-amount" />
-          <button id="mint-button" className="mint-button">
+          <label htmlFor="mint-amount">Amount:</label>
+          <input
+            type="number"
+            name="mintAmount"
+            className="mint-amount"
+            value={formData.mintAmount}
+            onChange={handleChange}
+          />
+          <button className="mint-button" onClick={handleMint}>
             Mint
           </button>
         </div>
 
-        <div class="section">
+        <div className="section">
           <h2>Melt Tokens</h2>
-          <label for="melt-invoice">Bolt11 Invoice:</label>
-          <input type="text" id="melt-invoice" className="melt-invoice" />
-          <button id="melt-button" className="melt-button">
+          <label htmlFor="melt-invoice">Bolt11 Invoice:</label>
+          <input
+            type="text"
+            name="meltInvoice"
+            className="melt-invoice"
+            value={formData.meltInvoice}
+            onChange={handleChange}
+          />
+          <button className="melt-button" onClick={handleMelt}>
             Melt
           </button>
         </div>
 
-        <div class="section">
+        <div className="section">
           <h2>Swap Tokens</h2>
-          <label for="swap-amount">Amount:</label>
-          <input type="number" id="swap-amount" className="swap-amount" />
-          <button id="swap-send-button" className="swap-send-button">
+          <label htmlFor="swap-amount">Amount:</label>
+          <input
+            type="number"
+            name="swapAmount"
+            className="swap-amount"
+            value={formData.swapAmount}
+            onChange={handleChange}
+          />
+          <button className="swap-send-button" onClick={handleSwapSend}>
             Swap to Send
           </button>
-          <label for="swap-token">Token:</label>
-          <input type="text" id="swap-token" className="swap-token" />
-          <button id="swap-claim-button" className="swap-claim-button">
+          <label htmlFor="swap-token">Token:</label>
+          <input
+            type="text"
+            name="swapToken"
+            className="swap-token"
+            value={formData.swapToken}
+            onChange={handleChange}
+          />
+          <button className="swap-claim-button" onClick={handleSwapClaim}>
             Swap to Claim
           </button>
         </div>
       </div>
 
       <div className="data-display-container">
-        <h2>Data Display</h2>
+        <h2>Balance: {balance}</h2>
         <pre id="data-output" className="data-output">
-          {}
+          {JSON.stringify(dataOutput, null, 2)}
         </pre>
       </div>
     </main>
